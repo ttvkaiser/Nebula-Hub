7dc05707ae66766b3b89ba932f7bc17cccadb333
ttvkaiser <vaeros694@gmail.com>
2025-07-25 11:12:59 -0600

    Update Main-config.lua

--- Configuration/Main-config.lua
+++ Configuration/Main-config.lua
@@ -1,1 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ttvkaiser/Nebula-Hub/refs/heads/main/Configuration/games%20white%20listed.lua"))()
 
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
