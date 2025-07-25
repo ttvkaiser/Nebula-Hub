b92f69d634b9999f95b45d7a67f767712bc7cbd1
ttvkaiser <vaeros694@gmail.com>
2025-07-25 11:17:02 -0600

    Update games white listed.lua

--- Configuration/games white listed.lua
+++ Configuration/games white listed.lua
@@ -1,1 +1,6 @@
+local Games = {
+  [3623096087] = "https://raw.githubusercontent.com/ttvkaiser/Nebula-Hub/refs/heads/main/Muscle-Legends/.lua", -- Muscle Legends
+  [3101667897] = "https://raw.githubusercontent.com/ttvkaiser/Nebula-Hub/refs/heads/main/Legends-Of-Speed/.lua", -- Legends of Speed
+}
 
+return Games
